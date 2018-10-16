22810702d2a03cdc8077f8e413a8d44c71b5a4c2
Vlasov Artem <avlasov@selectica.com>
2018-10-16 09:27:04 +0300

    Add with statement example

---
+++ src/main/kotlin/org/avlasov/idioms/with/WithExample.kt
@@ -0,0 +1,31 @@
+package org.avlasov.idioms.with
+
+import org.junit.Assert.assertEquals
+
+/**
+ *   Created By artemvlasov on 2018-10-16
+ **/
+class WithExample {
+
+    fun add(a: Int, b: Int): Int = a + b
+    fun subtract(a: Int, b: Int): Int = a - b
+    fun multiply(a: Int, b: Int): Int = a * b
+    fun divide(a: Int, b: Int): Int = a / b
+
+}
+
+fun main(args: Array<String>) {
+    val withExample = WithExample()
+    var result = 0
+    with(withExample) {
+        result =
+                divide(
+                    multiply(
+                        subtract(
+                            add(5, 5), 2
+                        ), 20
+                    ), 2
+                )
+    }
+    assertEquals(80, result)
+}
