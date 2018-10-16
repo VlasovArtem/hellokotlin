0965a6c2b679f776c4376c6f615b94513c515d96
Vlasov Artem <avlasov@selectica.com>
2018-10-16 08:34:37 +0300

    Lazy value example

---
+++ src/main/kotlin/org/avlasov/idioms/lazy/LazyExample.kt
@@ -0,0 +1,17 @@
+package org.avlasov.idioms.lazy
+
+import org.junit.Assert.assertEquals
+
+/**
+ *   Created By artemvlasov on 2018-10-16
+ **/
+class LazyExample {
+
+    val lazyA: String by lazy { "testA" }
+
+}
+
+fun main(args: Array<String>) {
+    val lazyExample = LazyExample()
+    assertEquals("testA", lazyExample.lazyA)
+}
