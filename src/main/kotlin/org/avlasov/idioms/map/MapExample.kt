4c91e26c1ea2480741a39bafa14e94331e0e270d
Vlasov Artem <avlasov@selectica.com>
2018-10-16 08:33:30 +0300

    Map usage example

---
+++ src/main/kotlin/org/avlasov/idioms/map/MapExample.kt
@@ -0,0 +1,15 @@
+package org.avlasov.idioms.map
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+
+/**
+ *   Created By artemvlasov on 2018-10-16
+ **/
+fun main(args: Array<String>) {
+    val mapOf = mapOf("a" to 1, "b" to 2, "c" to 3)
+    assertEquals(1, mapOf.get("a"))
+    assertNull(mapOf.get("d"))
+    assertEquals(2, mapOf["b"])
+    assertNull(mapOf["e"])
+}
