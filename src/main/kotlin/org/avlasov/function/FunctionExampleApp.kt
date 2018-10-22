f58e020ff2d93328875d1719000a377e6401e83d
Vlasov Artem <avlasov@selectica.com>
2018-10-22 21:03:52 +0300

    Update function example

--- src/main/kotlin/org/avlasov/function/FunctionExampleApp.kt
+++ src/main/kotlin/org/avlasov/function/FunctionExampleApp.kt
@@ -5,6 +5,18 @@ import org.junit.Assert.assertEquals
 /**
  *   Created By artemvlasov on 2018-10-12
  **/
+open class OpenClass {
+    open fun plusOne(first: Int = 10, second: Int = 10): Int = first + second + 1
+}
+
+class OriginalClass : OpenClass() {
+    override fun plusOne(first: Int, second: Int): Int = super.plusOne(first, second)
+}
+
+fun varargExample(vararg strings: String): Int = strings.size
+
+infix fun Int.shl2(x: Int): Int = x + 1
+
 class FunctionExampleApp {
 
     fun sum(a: Int, b: Int): Int {
@@ -27,4 +39,9 @@ fun main(arg: Array<String>) {
     assertEquals(9, sumExpression)
     functionExample.sumVoid(4, 5)
     functionExample.sumVoidWithReturn(4, 5)
+    assertEquals(12, OriginalClass().plusOne(second = 1))
+    assertEquals(23, OriginalClass().plusOne(22, 0))
+    assertEquals(3, varargExample(strings = *arrayOf("test", "test2", "test3")))
+    assertEquals(3, 1 shl2 2)
+    assertEquals(3, 1.shl2(2))
 }
